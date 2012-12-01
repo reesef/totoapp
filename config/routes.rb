3c71f58130a69b9b2ec9dcca65d3462cee7e9ed9
reesef <reesef@gmail.com>
2012-11-30 21:54:18 -0800

    Add splash page and begin styling

--- config/routes.rb
+++ config/routes.rb
@@ -55,8 +55,6 @@ Omrails::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-root :to => 'static_pages#home'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
