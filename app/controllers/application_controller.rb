452a8562e1d249970ed59f40da6f31be53d33af7
reesef <reesef@gmail.com>
2012-12-03 20:54:23 -0800

    Change redirect on sign in to lists_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	def after_sign_in_path_for(resource)                                                                                                                      
+   		lists_path                                                                                     
+    end            
+
+
+
+
 end
+
+
